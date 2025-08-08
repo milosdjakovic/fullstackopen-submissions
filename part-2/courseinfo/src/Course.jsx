f4c6ddcde1a61dff22e4b0a884f403975a8b4ac0
Milos Djakovic <32454231+milosdjakovic@users.noreply.github.com>
2025-08-08 19:59:47 +0200

    Part 2 Course Information part 8

--- part-2/courseinfo/src/Course.jsx
+++ part-2/courseinfo/src/Course.jsx
@@ -12,6 +12,15 @@ const Part = ({ name, exercises }) => {
     )
 }
 
+const Total = ({ parts }) => {
+    const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0)
+    return (
+        <p>
+            <strong>Total of {totalExercises} exercises</strong>
+        </p>
+    )
+}
+
 const Course = ({ course }) => {
     return (
         <div>
@@ -19,6 +28,7 @@ const Course = ({ course }) => {
             {course.parts.map(part => (
                 <Part key={part.id} name={part.name} exercises={part.exercises} />
             ))}
+            <Total parts={course.parts} />
         </div>
     )
 }
