466ffeecac898dd77eb1efff03e3cbf5d66116c8
Milos Djakovic <32454231+milosdjakovic@users.noreply.github.com>
2025-08-13 00:03:30 +0200

    Add environment variable for Open Weather API key

--- part-2/countries/src/services/weather.js
+++ part-2/countries/src/services/weather.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiKey = '9a313ad62887537b128600430007b298';
+const apiKey = import.meta.env.VITE_OPEN_WEATHER_API_KEY;
 const baseUrl = "https://api.openweathermap.org/data/2.5/weather";
 
 const getWeather = ({ lat, lon }) => {
