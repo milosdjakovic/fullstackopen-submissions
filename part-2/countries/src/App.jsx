b47d903bfcbb8b41f6e9ba7b5ee18a6e5e29df78
Milos Djakovic <32454231+milosdjakovic@users.noreply.github.com>
2025-08-12 20:52:16 +0200

    Fetch and filter coutnries "Exercises 2.18.-2.20."

--- part-2/countries/src/App.jsx
+++ part-2/countries/src/App.jsx
@@ -1,5 +1,33 @@
+import { useState, useEffect } from "react";
+
+import { getAllCountries } from "./services/countries";
+
 const App = () => {
-	return <div>Countries</div>;
+	const [country, setCountry] = useState(null);
+	const [countries, setCountries] = useState([]);
+
+	useEffect(() => {
+		getAllCountries().then((countries) => {
+			setCountries(countries);
+		});
+	}, []);
+
+	const handleChange = (event) => {
+		setCountry(event.target.value);
+	};
+
+	const filteredCountries = country
+		? countries.filter((c) =>
+				c.name.common.toLowerCase().includes(country.toLowerCase()),
+			)
+		: [];
+
+	return (
+		<div>
+			find countries: <input value={country} onChange={handleChange} />
+			<pre>{JSON.stringify(filteredCountries, null, 2)}</pre>
+		</div>
+	);
 };
 
 export default App;
